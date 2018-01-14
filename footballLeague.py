6de5ff16939c2dddee7945f1e4e11e1dbd184e58
Pechorin32 <dannymccluskey27@gmail.com>
2018-01-14 20:46:54 +0000

    Update footballLeague.py
    simplified and added while loop to allow continued input of matches

--- footballLeague.py
+++ footballLeague.py
@@ -3,43 +3,43 @@ league = {"Celtic": 3, "Rangers": 0, "Aberdeen": 0}
 homePoints = 0
 awayPoints = 0
 
-
-homeTeam = input("Please enter the home team: ")
-while homeTeam not in league:
-    print("Please enter a valid Team name: ")
+while True:
     homeTeam = input("Please enter the home team: ")
-if homeTeam in league:
-    existingHomePoints = league.get(homeTeam)
-    print("{} currently has {} points".format(homeTeam, existingHomePoints))
-    print()
+    if homeTeam == "quit":
+        break
+    if homeTeam in league:
+        existingHomePoints = league.get(homeTeam)
+        print("{} currently has {} points".format(homeTeam, existingHomePoints))
+    else:
+        print("We don't have a team called " + homeTeam)
 
-awayTeam = input("Please enter the away team: ")
-while awayTeam not in league:
-    print("Please enter a valid Team name: ")
     awayTeam = input("Please enter the away team: ")
-if awayTeam in league:
-    existingAwayPoints = league.get(awayTeam)
-    print("{} currently has {} points".format(awayTeam, existingAwayPoints))
+    while awayTeam not in league:
+        print("Please enter a valid Team name: ")
+        awayTeam = input("Please enter the away team: ")
+    if awayTeam in league:
+        existingAwayPoints = league.get(awayTeam)
+        print("{} currently has {} points".format(awayTeam, existingAwayPoints))
+        print()
+
+    homeScore = input("Please enter number of goals scored by {}: ".format(homeTeam))
+    awayScore = input("Please enter number of goals scored by {}: ".format(awayTeam))
+
+    if homeScore > awayScore:
+        homePoints += 3
+    elif homeScore == awayScore:
+        homePoints += 1
+        awayPoints += 1
+    elif homeScore < awayScore:
+        awayPoints += 3
+
+    print()
+    print("{} gets: {} points".format(homeTeam, homePoints))
+    print("{} gets: {} points".format(awayTeam, awayPoints))
     print()
 
-homeScore = input("Please enter number of goals scored by {}: ".format(homeTeam))
-awayScore = input("Please enter number of goals scored by {}: ".format(awayTeam))
+    totalHomePoints = int(existingHomePoints) +int(homePoints)
+    print("{} now has a total of {} points in the league".format(homeTeam, totalHomePoints))
 
-if homeScore > awayScore:
-    homePoints += 3
-elif homeScore == awayScore:
-    homePoints += 1
-    awayPoints += 1
-elif homeScore < awayScore:
-    awayPoints += 3
-
-print()
-print("{} gets: {} points".format(homeTeam, homePoints))
-print("{} gets: {} points".format(awayTeam, awayPoints))
-print()
-
-totalHomePoints = int(existingHomePoints) +int(homePoints)
-print("{} now has a total of {} points in the league".format(homeTeam, totalHomePoints))
-
-totalAwayPoints = int(existingAwayPoints) +int(awayPoints)
-print("{} now has a total of {} points in the league".format(awayTeam, totalAwayPoints))
+    totalAwayPoints = int(existingAwayPoints) +int(awayPoints)
+    print("{} now has a total of {} points in the league".format(awayTeam, totalAwayPoints))

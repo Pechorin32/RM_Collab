59150feb00928c2cae5a1a3fa9ff63fc6deb251a
Pechorin32 <dannymccluskey27@gmail.com>
2018-01-24 20:31:21 +0000

    further branch added to the code
    Realised programme was buggy when a team played more than one game during a week. When this was the case the value assigned to the homePoints/awayPoints variable through lines 39-45 was the value against that teams key in the dict plus the number of points gained that week already plus a further 3 or 1 points
    Amended lines 39-45 with new variables.
    
     Still buggy as the while loop means that until you exit the code the same teams scores being entered more than once carries the value against the homePoints/awayPoints variable in to the next loop

--- footballLeague.py
+++ footballLeague.py
@@ -5,25 +5,28 @@ This script automates the process of logging football match scores and assigning
 
 """
 
-league = {"Celtic": 3, "Rangers": 0, "Aberdeen": 0} #create dict object to store football league teams and their points. 
+league = {"CELTIC": 3, "RANGERS": 0, "ABERDEEN": 0}
+# create dict object to store football league teams and their points.
 
 homePoints = 0 # create variable to store home points as an integer. Set to zero at point of object creation.
 awayPoints = 0 # create variable to store away points as an integer. Set to zero at point of object creation.
+postGameHomePoints = 0
+postGameAwayPoints = 0
 
 while True: #start infinite loop - this is useful where continuous operations are required that are broken or interupted only when certain input is provided.
-    homeTeam = input("Please enter the home team: ") #homeTeam variable stores user input. Syntax suggests Python 3. Is that right?
-    if homeTeam == "quit": #if user types the string "quit" 
+    homeTeam = input("Please enter the home team: ").upper() #homeTeam variable stores user input. Syntax suggests Python 3. Is that right?
+    if homeTeam == "quit": #if user types the string "quit"
         break #interupt the infinite loop
     if homeTeam in league: #see if user input string matches an entry in the league dict.
-        #SUGGESTION from MR: for reference to league dict in line 17, be explicit about the fact it's the dict KEYS your looking in for a match with homeTeam variable. Use league.keys() 
+        #SUGGESTION from MR: for reference to league dict in line 17, be explicit about the fact it's the dict KEYS your looking in for a match with homeTeam variable. Use league.keys()
         existingHomePoints = league.get(homeTeam) # get the value associated with the key in the dict that matches the homeTeam variable.
         print("{} currently has {} points".format(homeTeam, existingHomePoints)) # print some facts for the user. Modern formatting syntax employed.
     else:
         print("We don't have a team called " + homeTeam) #some simple error handling to deal with user entering a team that isn't in the league.
 
-    awayTeam = input("Please enter the away team: ") 
-    if awayTeam == "quit":  
-        break 
+    awayTeam = input("Please enter the away team: ").upper()
+    if awayTeam == "quit":
+        break
     if awayTeam in league:
         existingAwayPoints = league.get(awayTeam)
         print("{} currently has {} points".format(awayTeam, existingAwayPoints))
@@ -34,30 +37,28 @@ while True: #start infinite loop - this is useful where continuous operations ar
     awayScore = input("Please enter number of goals scored by {}: ".format(awayTeam)) # awayScore variable stores user input
 
     if homeScore > awayScore: # compares variables
-        homePoints += 3 # when above condition is met adds three points to the existing number of points held by the home team
+        postGameHomePoints = homePoints + 3# when above condition is met adds three points to the existing number of points held by the home team
+        postGameAwayPoints = awayPoints + 0
     elif homeScore == awayScore: # compares variables
-        homePoints += 1 # when above condition is met adds one point to the existing number of points held by the home team
-        awayPoints += 1 # when above condition is met adds one point to the existing number of points held by the away team
+        postGameHomePoints = homePoints + 1 # when above condition is met adds one point to the existing number of points held by the home team
+        postGameAwayPoints = awayPoints + 1 # when above condition is met adds one point to the existing number of points held by the away team
     elif homeScore < awayScore: # compares variables
-        awayPoints += 3 # when above condition is met adds three points to the existing number of points held by the away team
+        postGameAwayPoints = awayPoints + 3 # when above condition is met adds three points to the existing number of points held by the away team
+        ostGameHomePoints = homePoints + 0
 
     print()
-    print("{} gets: {} points".format(homeTeam, homePoints))
-    print("{} gets: {} points".format(awayTeam, awayPoints))
+    print("{} gets: {} points".format(homeTeam, postGameHomePoints))
+    print("{} gets: {} points".format(awayTeam, postGameAwayPoints))
     print()
     # prints number of points for user to view.
 
-    totalHomePoints = int(existingHomePoints) +int(homePoints) # calculates the number of points the home team now has in the league
+    totalHomePoints = int(existingHomePoints) +int(postGameHomePoints) # calculates the number of points the home team now has in the league
     print("{} now has a total of {} points in the league".format(homeTeam, totalHomePoints))
 
-    totalAwayPoints = int(existingAwayPoints) +int(awayPoints) # calculates the number of points the home team now has in the league
+    totalAwayPoints = int(existingAwayPoints) +int(postGameAwayPoints) # calculates the number of points the home team now has in the league
     print("{} now has a total of {} points in the league".format(awayTeam, totalAwayPoints))
-    
+
     league[homeTeam] = totalHomePoints # updates the dict object with the number of points
     league[awayTeam] = totalAwayPoints
 
-    print(league) 
-
-
-
-
+    print(league)
